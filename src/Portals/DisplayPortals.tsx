f7ae73841fe5b6d84ebb94806844c8051f96def4
ackermannQ <ackermann.quentin@gmail.com>
2023-03-17 17:55:29 -0400

    Adding images for the portals and server selection

--- src/Portals/DisplayPortals.tsx
+++ src/Portals/DisplayPortals.tsx
@@ -1,27 +1,70 @@
-import { Typography } from "@mui/material";
+import { Autocomplete, TextField, Typography } from "@mui/material";
+import Box from "@mui/system/Box";
 import React from "react";
 
 import { IPortal } from ".";
 import { Dimensions } from "./Dimensions";
+import { getServersList } from "./Servers";
 
 export default function DisplayPortals(props: {portals: IPortal[] | null}) {
+  const [serversList, setServersList] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    setServersList(getServersList());
+  }, []);
+
   return props.portals &&
-    <div>
+  <>
+    <Box sx={{
+      backgroundColor: "primary.dark",
+      "&:hover": {
+        backgroundColor: "primary.main",
+        opacity: [0.9, 0.8, 0.7],
+      },
+      margin: 1,
+    }}>
+      <ServerSelection servers={serversList} />
+      {props.portals[0].server}
+    </Box>
+    <div style={{backgroundColor: "#1D1D1D", display: "flex", marginRight: 50}}>
       {props.portals.map(portal => {
-        return <>
-          <Typography sx={{ fontSize: 26, color: "white" }} gutterBottom>
-            {capitalizeFirstLetter(portal.dimension)}
-          </Typography>
+        return <Box key={portal.dimension} sx={{
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          margin: 1,
+        }}>
+          <img width={50} style={{cursor: "pointer"}} src={`${portal.dimension}.png`} alt={capitalizeFirstLetter(portal.dimension)} onClick={() => navigator.clipboard.writeText("salut")}/>
           {portal?.position?.x ? <Typography sx={{ fontSize: 18 }}>[{portal?.position?.x}, {portal?.position?.y}]</Typography> : "Indisponible"}
           <Typography sx={{ fontSize: 14 }} color="#A0A2A4" gutterBottom>
             {portal.remainingUses} utilisations restantes
           </Typography>
-        </>;
+        </Box>;
       })}
-    </div>;
+    </div>
+  </>;
 }
 
 function capitalizeFirstLetter(dimension: Dimensions) {
   const string = dimension.toString();
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function ServerSelection(props: {servers: string[]}) {
+  const [currentServer, setCurrentServer] = React.useState<string | null>(props.servers?.[0]);
+
+  return (
+    <Autocomplete
+      value={currentServer}
+      onChange={(event: any, newValue: string | null) => {
+        setCurrentServer(newValue);
+      }}
+      id="controllable-states-demo"
+      options={props.servers}
+      sx={{ width: 200, backgroundImage:`url(/${currentServer}.jpg)`, backgroundRepeat:"no-repeat", backgroundPositionY: "30%"}}
+      renderInput={(params) => <TextField sx={{backgroundColor: "rgba(0, 0, 0, 0.5)"}} {...params} />}
+    />
+  );
 }
--- src/Portals/Servers.ts
+++ src/Portals/Servers.ts
@@ -2,3 +2,15 @@ export enum Servers {
     hellmina,
     orukam,
 }
+
+export function getServersList() {
+  const servers = [];
+  for (const server in Servers) {
+    const isValueProperty = Number(server) >= 0;
+    if (isValueProperty) {
+      servers.push(Servers[server]);
+    }
+  }
+
+  return servers;
+}
